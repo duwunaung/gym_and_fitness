f140ce1ef46f6682c7d32e82b0e73ab7deecb663
Pi Rson <duwunaung@gmail.com>
2024-04-20 03:57:18 +0630

    Update settings.py

--- gym_and_fitness/settings.py
+++ gym_and_fitness/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 
 STATICFILES_DIRS = [BASE_DIR/'staticfiles',]
 STATIC_ROOT = BASE_DIR/'staticfiles'

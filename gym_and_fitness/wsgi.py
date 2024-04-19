4334fca83a00ed189586dc09d6a45cd7700bce6e
Pi Rson <duwunaung@gmail.com>
2024-04-20 04:11:07 +0630

    Add whitenoice

--- gym_and_fitness/wsgi.py
+++ gym_and_fitness/wsgi.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gym_and_fitness.settings")
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root="/staticfiles")
 app = application
